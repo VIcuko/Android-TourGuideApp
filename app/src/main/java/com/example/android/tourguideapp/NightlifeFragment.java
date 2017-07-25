24a3cdfdadfc15b2a12a976d712e824135c563f6
Vicuko <victorc.fernandez@gmail.com>
2017-07-26 01:52:00 +0200

    Update NightLifeFragment to new Site content

--- app/src/main/java/com/example/android/tourguideapp/NightlifeFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/NightlifeFragment.java
@@ -1,125 +1,48 @@
 package com.example.android.tourguideapp;
 
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-
-import static android.media.AudioManager.AUDIOFOCUS_GAIN;
-import static android.media.AudioManager.AUDIOFOCUS_LOSS;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NightlifeFragment extends Fragment {
 
-    private MediaPlayer mMediaPlayer;
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mp) {
-            releaseMediaPlayer();
-        }
-    };
-    private AudioManager mAudioManager;
-
-    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
-            new AudioManager.OnAudioFocusChangeListener() {
-                public void onAudioFocusChange(int focusChange) {
-                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-                            focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-                        mMediaPlayer.pause();
-                        mMediaPlayer.seekTo(0);
-                    } else if (focusChange == AUDIOFOCUS_GAIN) {
-                        mMediaPlayer.start();
-                    } else if (focusChange == AUDIOFOCUS_LOSS) {
-                        releaseMediaPlayer();
-                    }
-                }
-            };
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-
     public NightlifeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.site_list, container, false);
 
-        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
-
+        //Create an ArrayList of sites
         final ArrayList<Site> sites = new ArrayList<Site>();
+        sites.add(new Site("Puerta de Alcalá", "lutti", R.drawable.ic_place_white_24dp, R.string.monument_1));
+        sites.add(new Site("Cibele", "otiiko", R.drawable.ic_place_white_24dp, R.string.monument_2));
+        sites.add(new Site("Bernabéu", "tolookosu", R.drawable.ic_place_white_24dp, R.string.monument_3));
+        sites.add(new Site("Puerta de Sol", "oyyisa", R.drawable.ic_place_white_24dp, R.string.monument_4));
+        sites.add(new Site("Plaza Mayor", "massokka", R.drawable.ic_place_white_24dp, R.string.monument_5));
+        sites.add(new Site("Palacio Real", "temmokka", R.drawable.ic_place_white_24dp, R.string.monument_6));
+        sites.add(new Site("Parque el retiro", "kenekaku", R.drawable.ic_place_white_24dp, R.string.monument_7));
+        sites.add(new Site("Museo del Prado", "kawinta", R.drawable.ic_place_white_24dp, R.string.monument_8));
+        sites.add(new Site("Gran Vía", "wo’e", R.drawable.ic_place_white_24dp, R.string.monument_9));
+        sites.add(new Site("Basílica de la Almudena", "na’aacha", R.drawable.ic_place_white_24dp, R.string.monument_10));
 
-        sites.add(new Site("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
-        sites.add(new Site("What is your name?", "tinnә oyaase'nә", R.raw.phrase_what_is_your_name));
-        sites.add(new Site("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
-        sites.add(new Site("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
-        sites.add(new Site("I’m feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
-        sites.add(new Site("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
-        sites.add(new Site("Yes, I’m coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
-        sites.add(new Site("I’m coming.", "әәnәm", R.raw.phrase_im_coming));
-        sites.add(new Site("Let’s go.", "yoowutis", R.raw.phrase_lets_go));
-        sites.add(new Site("Come here.", "әnni'nem", R.raw.phrase_come_here));
-
-        SiteAdapter adapter = new SiteAdapter(getActivity(), sites, R.color.category_phrases);
+        SiteAdapter adapter = new SiteAdapter(getActivity(), sites, R.color.category_monuments);
 
         ListView listView = (ListView) rootView.findViewById(R.id.list);
 
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                releaseMediaPlayer();
-                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
-                        AudioManager.STREAM_MUSIC,
-                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    mMediaPlayer = MediaPlayer.create(getActivity(), sites.get(position).getAudioResourceId());
-                    mMediaPlayer.start();
-                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
-                }
-            }
-        });
-
         return rootView;
     }
-
-    /**
-     * Clean up the media player by releasing its resources.
-     */
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mMediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            mMediaPlayer.release();
-
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mMediaPlayer = null;
-
-            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-        }
-    }
-
 }
