c318d7d52371d50a08abb6ae6c69dafd31507439
Vicuko <victorc.fernandez@gmail.com>
2017-07-26 01:42:05 +0200

    Edit sites content in MonumentsFragment

--- app/src/main/java/com/example/android/tourguideapp/MonumentsFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/MonumentsFragment.java
@@ -26,18 +26,18 @@ public class MonumentsFragment extends Fragment {
 
         //Create an ArrayList of sites
         final ArrayList<Site> sites = new ArrayList<Site>();
-        sites.add(new Site("Puerta de Alcalá", "lutti", R.drawable.ic_place_white_24dp, R.raw.number_one));
-        sites.add(new Site("Cibele", "otiiko", R.drawable.ic_place_white_24dp, R.raw.number_two));
-        sites.add(new Site("Bernabéu", "tolookosu", R.drawable.ic_place_white_24dp, R.raw.number_three));
-        sites.add(new Site("Puerta de Sol", "oyyisa", R.drawable.ic_place_white_24dp, R.raw.number_four));
-        sites.add(new Site("Plaza Mayor", "massokka", R.drawable.ic_place_white_24dp, R.raw.number_five));
-        sites.add(new Site("Palacio Real", "temmokka", R.drawable.ic_place_white_24dp, R.raw.number_six));
-        sites.add(new Site("Parque el retiro", "kenekaku", R.drawable.ic_place_white_24dp, R.raw.number_seven));
-        sites.add(new Site("Museo del Prado", "kawinta", R.drawable.ic_place_white_24dp, R.raw.number_eight));
-        sites.add(new Site("Gran Vía", "wo’e", R.drawable.ic_place_white_24dp, R.raw.number_nine));
-        sites.add(new Site("Basílica de la Almudena", "na’aacha", R.drawable.ic_place_white_24dp, R.raw.number_ten));
+        sites.add(new Site("Puerta de Alcalá", "lutti", R.drawable.ic_place_white_24dp, R.string.monument_1));
+        sites.add(new Site("Cibele", "otiiko", R.drawable.ic_place_white_24dp, R.string.monument_2));
+        sites.add(new Site("Bernabéu", "tolookosu", R.drawable.ic_place_white_24dp, R.string.monument_3));
+        sites.add(new Site("Puerta de Sol", "oyyisa", R.drawable.ic_place_white_24dp, R.string.monument_4));
+        sites.add(new Site("Plaza Mayor", "massokka", R.drawable.ic_place_white_24dp, R.string.monument_5));
+        sites.add(new Site("Palacio Real", "temmokka", R.drawable.ic_place_white_24dp, R.string.monument_6));
+        sites.add(new Site("Parque el retiro", "kenekaku", R.drawable.ic_place_white_24dp, R.string.monument_7));
+        sites.add(new Site("Museo del Prado", "kawinta", R.drawable.ic_place_white_24dp, R.string.monument_8));
+        sites.add(new Site("Gran Vía", "wo’e", R.drawable.ic_place_white_24dp, R.string.monument_9));
+        sites.add(new Site("Basílica de la Almudena", "na’aacha", R.drawable.ic_place_white_24dp, R.string.monument_10));
 
-        WordAdapter adapter = new WordAdapter(getActivity(), sites, R.color.category_numbers);
+        WordAdapter adapter = new WordAdapter(getActivity(), sites, R.color.category_monuments);
 
         ListView listView = (ListView) rootView.findViewById(R.id.list);
 
@@ -45,5 +45,4 @@ public class MonumentsFragment extends Fragment {
 
         return rootView;
     }
-
 }
