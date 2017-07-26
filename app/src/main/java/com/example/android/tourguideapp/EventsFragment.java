9e43ac5c1f0a84fc5e302fa74b86d4a752397f26
Vicuko <victorc.fernandez@gmail.com>
2017-07-26 21:09:45 +0200

    Introduce Events data

--- app/src/main/java/com/example/android/tourguideapp/EventsFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/EventsFragment.java
@@ -27,16 +27,39 @@ public class EventsFragment extends Fragment {
 
         //Create an ArrayList of sites
         final ArrayList<Site> sites = new ArrayList<Site>();
-        sites.add(new Site("Puerta de Alcalá", "lutti", R.drawable.ic_place_white_24dp, R.string.monument_1,10));
-        sites.add(new Site("Cibele", "otiiko", R.drawable.ic_place_white_24dp, R.string.monument_2,0));
-        sites.add(new Site("Bernabéu", "tolookosu", R.drawable.ic_place_white_24dp, R.string.monument_3,45));
-        sites.add(new Site("Puerta de Sol", "oyyisa", R.drawable.ic_place_white_24dp, R.string.monument_4,56));
-        sites.add(new Site("Plaza Mayor", "massokka", R.drawable.ic_place_white_24dp, R.string.monument_5,89));
-        sites.add(new Site("Palacio Real", "temmokka", R.drawable.ic_place_white_24dp, R.string.monument_6,0));
-        sites.add(new Site("Parque el retiro", "kenekaku", R.drawable.ic_place_white_24dp, R.string.monument_7,15));
-        sites.add(new Site("Museo del Prado", "kawinta", R.drawable.ic_place_white_24dp, R.string.monument_8,12));
-        sites.add(new Site("Gran Vía", "wo’e", R.drawable.ic_place_white_24dp, R.string.monument_9,45));
-        sites.add(new Site("Basílica de la Almudena", "na’aacha", R.drawable.ic_place_white_24dp, R.string.monument_10,32));
+
+        sites.add(new Site("Flamenco show", "Feel the heat of the traditional dance of Spain with a flamenco show " +
+                "in one of the most prestigious \"tablaos\" (flamenco halls) in the country.",
+                R.drawable.ic_place_white_24dp, R.string.monument_1,49));
+
+        sites.add(new Site("Madrid walking tour", "Admire in centuries of Madrid's history during a guided tour on " +
+                "foot through the Spanish capital.", R.drawable.ic_place_white_24dp, R.string.monument_2,90));
+
+        sites.add(new Site("Real Madrid match", "Let yourself be carried away by football fever in a Real Madrid match " +
+                "at the Santiago Bernabéu stadium.", R.drawable.ic_place_white_24dp, R.string.monument_3,800));
+
+        sites.add(new Site("Full day excursion to Toledo ", "Travel from Madrid to Toledo, a national monument of Spain's " +
+                "art and history, and discover the treasure of El Greco's works of art.", R.drawable.ic_place_white_24dp,
+                R.string.monument_4,57));
+
+        sites.add(new Site("One day tour at El Escorial", "Discover the magnificent monastery of El Escorial, the Valley of " +
+                "the Fallen and visit the \"Imperial City\" of Toledo on this day trip.", R.drawable.ic_place_white_24dp,
+                R.string.monument_5,97));
+
+        sites.add(new Site("Free stops bus", "Discover the highlights of Madrid aboard a double decker bus with free stops.",
+                R.drawable.ic_place_white_24dp, R.string.monument_6,21));
+
+        sites.add(new Site("Visit the Prado Museum", "Explore the Prado Museum in Madrid with an expert guide in art, avoiding " +
+                "long entry lines with your priority access ticket.", R.drawable.ic_place_white_24dp, R.string.monument_7,40));
+
+        sites.add(new Site("3-hour bicycle tour", "Enjoy this 3-hour bike tour that will give you the opportunity to see the best " +
+                "places of interest in Madrid in a fun way.", R.drawable.ic_place_white_24dp, R.string.monument_8,22));
+
+        sites.add(new Site("Arab baths experience", "Look for serenity in the Arab baths of Madrid, Hamman Al Ándalus, and spend " +
+                "time relaxing your body, soul and mind.", R.drawable.ic_place_white_24dp, R.string.monument_9,63));
+
+        sites.add(new Site("Guided wine tour", "Discover the prestigious wine region of the Ribera del Duero from Madrid.",
+                R.drawable.ic_place_white_24dp, R.string.monument_10,150));
 
         SiteAdapter adapter = new SiteAdapter(getActivity(), sites, R.color.category_events);
 
